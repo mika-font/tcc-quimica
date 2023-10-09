f0d484b19519998e21e99ab4952d55d01be2a882
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-10-08 23:13:00 -0300

    Atualizando o visualizar questionário

--- visualizar_questionario.php
+++ visualizar_questionario.php
@@ -1,6 +1,10 @@
 <?php
 include_once('controle.php');
-$id_questionario = $_GET['questionario'];
+if (isset($_GET['questionario'])) {
+    $id_questionario = $_GET['questionario'];
+} else {
+    $id_questionario = $_POST['id_questionario'];
+}
 $sql = "SELECT * FROM questionario WHERE id_questionario = $id_questionario";
 $sql2 = "SELECT * FROM questao INNER JOIN contem ON questao.id_questao = contem.id_questao WHERE contem.id_questionario = $id_questionario";
 $result1 = mysqli_query($conexao, $sql);
@@ -8,13 +12,12 @@ $result2 = mysqli_query($conexao, $sql2);
 
 if ($result1 == TRUE && $result2 == TRUE) {
     $info_questionario = mysqli_fetch_assoc($result1);
-    $questions = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+    $questions = mysqli_fetch_all($result2, MYSQLI_ASSOC); 
 } else {
     echo mysqli_errno($conexao) . ": " . mysqli_error($conexao);
 }
 
-if(isset($_POST['enviar'])){
-    $respostas = mysqli_fetch_assoc($result2);
+if (isset($_POST['enviar'])) {
     $contagem = 0;
     $r_quest_1 = $_POST['questao1'];
     $r_quest_2 = $_POST['questao2'];
@@ -26,71 +29,67 @@ if(isset($_POST['enviar'])){
     $r_quest_8 = $_POST['questao8'];
     $r_quest_9 = $_POST['questao9'];
     $r_quest_10 = $_POST['questao10'];
-    if($r_quest_1 == $respostas[0]['alt_correta']){
+    if ($r_quest_1 == $questions[0]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_2 == $respostas[1]['alt_correta']){
+    if ($r_quest_2 == $questions[1]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_3 == $respostas[2]['alt_correta']){
+    if ($r_quest_3 == $questions[2]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_4 == $respostas[3]['alt_correta']){
+    if ($r_quest_4 == $questions[3]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_5 == $respostas[4]['alt_correta']){
+    if ($r_quest_5 == $questions[4]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     }
-    if($r_quest_6 == $respostas[5]['alt_correta']){
-        $contagem++;
-        //estilizar o correto com js
-    } else {
-        //estilizar o errado com js
-    }
-    if($r_quest_7 == $respostas[6]['alt_correta']){
+    if ($r_quest_6 == $questions[5]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_8 == $respostas[7]['alt_correta']){
+    if ($r_quest_7 == $questions[6]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_9 == $respostas[8]['alt_correta']){
+    if ($r_quest_8 == $questions[7]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    if($r_quest_10 == $respostas[9]['alt_correta']){
+    if ($r_quest_9 == $questions[8]['alt_correta']) {
         $contagem++;
         //estilizar o correto com js
     } else {
         //estilizar o errado com js
     }
-    echo $contagem;
-    //modal de quantas questões acertou e quantas errou.
-    //enviar resposta para o email do professor
+    if ($r_quest_10 == $questions[9]['alt_correta']) {
+        $contagem++;
+        //estilizar o correto com js
+    } else {
+        //estilizar o errado com js
+    }
 }
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -108,6 +107,7 @@ if(isset($_POST['enviar'])){
     <?php include_once('cabecalho.php'); ?>
     <main class="container pb-3">
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <input type="hidden" name="id_questionario" value="<?= $info_questionario['id_questionario'] ?>">
             <div class="row">
                 <div class="col">
                     <div class="text-center py-3">
@@ -133,10 +133,10 @@ if(isset($_POST['enviar'])){
                                     <img src="<?php echo $pergunta['imagem'] ?>" height="200px" width="auto">
                                 <?php endif ?>
                                 <div class="text-start">
-                                    <p><input type="radio" name="questao<?php echo $i; ?>" value="alt1"> a) <?php echo $pergunta['alt_1'] ?></p>
-                                    <p><input type="radio" name="questao<?php echo $i; ?>" value="alt2"> b) <?php echo $pergunta['alt_2'] ?></p>
-                                    <p><input type="radio" name="questao<?php echo $i; ?>" value="alt3"> c) <?php echo $pergunta['alt_3'] ?></p>
-                                    <p><input type="radio" name="questao<?php echo $i; ?>" value="alt4"> d) <?php echo $pergunta['alt_4'] ?></p>
+                                    <p><label><input type="radio" name="questao<?php echo $i; ?>" value="alt1" required> a) <?php echo $pergunta['alt_1'] ?></label></p>
+                                    <p><label><input type="radio" name="questao<?php echo $i; ?>" value="alt2" required> b) <?php echo $pergunta['alt_2'] ?></label></p>
+                                    <p><label><input type="radio" name="questao<?php echo $i; ?>" value="alt3" required> c) <?php echo $pergunta['alt_3'] ?></label></p>
+                                    <p><label><input type="radio" name="questao<?php echo $i; ?>" value="alt4" required> d) <?php echo $pergunta['alt_4'] ?></label></p>
                                 </div>
                             </div>
                         </div>
@@ -149,11 +149,28 @@ if(isset($_POST['enviar'])){
                 <?php $i++;
                 endforeach ?>
             </div>
+            <div class="row">
+                <div class="col">
+                    <div class="text-end py-3">
+                        <input class="btn link-body-emphasis text-light" type="submit" name="enviar" style="background-color: var(--color-purple);">
+                    </div>
+                </div>
+            </div>
         </form>
-        <div class="row">
-            <div class="col">
-                <div class="text-end py-3">
-                    <input class="btn link-body-emphasis text-light" type="submit" name="enviar"  style="background-color: var(--color-purple);">
+        <div class="modal fade" id="resposta" aria-labelledby="respostaLabel">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="resposaLabel">Seu desempenho no questionário foi o seguinte!</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Você acertou <?php echo $contagem ?>/10!</p>
+                        <p>Suas respostas serão enviadas ao professor via email.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn text-light" data-bs-dismiss="modal" style="background-color: var(--color-purple);">Fechar</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -161,6 +178,14 @@ if(isset($_POST['enviar'])){
     <?php include_once('rodape.php'); ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script>
+        <?php if (isset($_POST['enviar'])) : ?>
+            document.addEventListener("DOMContentLoaded", (event) => {
+                var myModal = new bootstrap.Modal(document.getElementById('resposta'));
+                myModal.show();
+            });
+        <?php endif ?>
+    </script>
 </body>
 
 </html>
