044170afdbe09f265d2cde3911b0552a87150f63
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-11-08 19:27:05 -0300

    Início do gerar pdf
    Funcionalidade a ser repensada

--- conteudos/introducao.php
+++ conteudos/introducao.php
@@ -1,13 +1,12 @@
-<?php include_once('../controle.php') ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+<?php include_once('../controle.php') ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/css/pacote.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="shortcut icon" href="../assets/img-sistem/atomo.ico" type="image/x-icon">
     <title>Introdução à Química Forense</title>
 </head>
@@ -25,21 +24,31 @@
         </div>
         <div class="row">
             <div class="col-xl-12">
-                <p>No último século, a Ciência Forense tornou-se crucial como ferramenta de resolução de crimes e na busca pela verdade nos campos da justiça. Nela, a Química Forense ganha destaque, um ramo da química que se preocupa em auxiliar a investigação forense na procura por respostas verídicas para atender aspectos de interesse judiciário, assim volta-se para a análise e classificação de substâncias encontradas na perícia de um local.</p>
-                <img src="">
-                <p>A química forense possui áreas voltadas à extrair, examinar e classificar diferentes tipos de substâncias encontradas em um cenário criminal, como Toxicologia, Análise de Drogas, Documentoscopia, Espectroscopia, Análise de Tintas, Material Residuográfico, Resíduos de Incêndio, Análise de Vestígios Biológicos e Balística, além de estar intrinsecamente relacionada à outros âmbitos da ciência como, Psicologia, Biologia, Física, Matemática, Química, Sociologia e Farmacologia, a fim de auxiliar na resolução eficaz de um caso criminal.</p>
-                <p>Ademais, a química forense não somente é aplicada nas perícias policiais, mas também nas perícias trabalhistas, perícias industriais, perícias ambientais e doping desportivo.</p>
-                <img src="">
-                <img src="">
-                <p>Mas o que é perícia? Perícia é o exame realizado pelo técnico para verificar e esclarecer um fato por meio do estudo de um objeto, ou seja, a química forense pode e deve ser utilizada na avaliação de produtos industriais, como alimentos e medicamentos, examinação de cenários ambientais e trabalhistas, além do uso de substâncias ilegais durante a prática de um esporte.</p>
-                <p>O uso dos conhecimentos científicos para a análise de provas do crime iniciou-se desde os primórdios da sociedade, como na Roma Antiga, onde eram comuns os casos de envenenamento de figuras proeminentes do mundo político, resultando na utilização de conhecimentos científicos para encontrar o real culpado.</p>
-                <p>Está vendo como a química forense é uma ciência incrível? Por mais que abordada em documentários, produções cinematográficas, midiáticas e literárias, essa área possui uma escassa literatura, prejudicando e enfraquecendo a relação entre a sociedade, no exercício pleno de sua cidadania, e a real aplicabilidade da química forense na investigação criminal.</p>
-                <img src="">
+                <div class="text-wrap">
+                    <p>No último século, a Ciência Forense tornou-se crucial como ferramenta de resolução de crimes e na busca pela verdade nos campos da justiça. Nela, a Química Forense ganha destaque, um ramo da química que se preocupa em auxiliar a investigação forense na procura por respostas verídicas para atender aspectos de interesse judiciário, assim volta-se para a análise e classificação de substâncias encontradas na perícia de um local.</p>
+                    <div class="text-center">
+                        <img src="">
+                    </div>
+                    <p>A química forense possui áreas voltadas à extrair, examinar e classificar diferentes tipos de substâncias encontradas em um cenário criminal, como Toxicologia, Análise de Drogas, Documentoscopia, Espectroscopia, Análise de Tintas, Material Residuográfico, Resíduos de Incêndio, Análise de Vestígios Biológicos e Balística, além de estar intrinsecamente relacionada à outros âmbitos da ciência como, Psicologia, Biologia, Física, Matemática, Química, Sociologia e Farmacologia, a fim de auxiliar na resolução eficaz de um caso criminal.</p>
+                    <p>Ademais, a química forense não somente é aplicada nas perícias policiais, mas também nas perícias trabalhistas, perícias industriais, perícias ambientais e doping desportivo.</p>
+                    <div class="text-center">
+                        <img src="">
+                    </div>
+                    <div class="text-center">
+                        <img src="">
+                    </div>
+                    <p>Mas o que é perícia? Perícia é o exame realizado pelo técnico para verificar e esclarecer um fato por meio do estudo de um objeto, ou seja, a química forense pode e deve ser utilizada na avaliação de produtos industriais, como alimentos e medicamentos, examinação de cenários ambientais e trabalhistas, além do uso de substâncias ilegais durante a prática de um esporte.</p>
+                    <p>O uso dos conhecimentos científicos para a análise de provas do crime iniciou-se desde os primórdios da sociedade, como na Roma Antiga, onde eram comuns os casos de envenenamento de figuras proeminentes do mundo político, resultando na utilização de conhecimentos científicos para encontrar o real culpado.</p>
+                    <p>Está vendo como a química forense é uma ciência incrível? Por mais que abordada em documentários, produções cinematográficas, midiáticas e literárias, essa área possui uma escassa literatura, prejudicando e enfraquecendo a relação entre a sociedade, no exercício pleno de sua cidadania, e a real aplicabilidade da química forense na investigação criminal.</p>
+                    <div class="text-center">
+                        <img src="">
+                    </div>
+                <div>
             </div>
         </div>
         <div class="row">
             <div class="col-xl-12 p-2 text-end">
-                <a href="../gerarpdf.php" class="btn link-body-emphasis text-light" style="background-color: var(--color-purple);">Gerar PDF</a>
+                <a href="gerarpdf.php" class="btn link-body-emphasis text-light" style="background-color: var(--color-purple);">Gerar PDF</a>
             </div>
         </div>
     </main>
