a78b3aafb00543f115b50e4efcf8e8a63fea512a
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-11-08 15:31:25 -0300

    Validar questões

--- cad_questionario.php
+++ cad_questionario.php
@@ -1,6 +1,6 @@
 <?php
 include_once('controle.php');
-$sql = "SELECT * FROM questao"; 
+$sql = "SELECT * FROM questao";
 $result = mysqli_query($conexao, $sql);
 $questoes = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
@@ -28,7 +28,7 @@ $questoes = mysqli_fetch_all($result, MYSQLI_ASSOC);
                     <h2 class="h2">Cadastrar Questionário</h2>
                     <div class="m-3"><i class="fa-solid fa-clipboard-question fa-2xl" style="color: #000000;"></i></div>
                     <hr>
-                </div>   
+                </div>
             </div>
         </div>
         <div class="row">
@@ -46,8 +46,8 @@ $questoes = mysqli_fetch_all($result, MYSQLI_ASSOC);
                     </div>
                     <div class="row">
                         <div class="col-xl-6 py-2">
-                           <label class="form-label">Data de Início:</label> 
-                           <input class="form-control" type="date" name="data_inicio" required>
+                            <label class="form-label">Data de Início:</label>
+                            <input class="form-control" type="date" name="data_inicio" required>
                         </div>
                         <div class="col-xl-6 py-2">
                             <label class="form-label">Data de Término:</label>
@@ -60,10 +60,10 @@ $questoes = mysqli_fetch_all($result, MYSQLI_ASSOC);
                         </div>
                     </div>
                     <div class="row">
-                        <?php 
+                        <?php
                         $limite = 2;
                         $count = 0;
-                        foreach ($questoes as $questao) : 
+                        foreach ($questoes as $questao) :
                             $count++;
                         ?>
                             <div class="col-xl-6 py-3">
@@ -81,30 +81,30 @@ $questoes = mysqli_fetch_all($result, MYSQLI_ASSOC);
                                             <p><?php echo "b) " . $questao['alt_2'] ?></p>
                                             <p><?php echo "c) " . $questao['alt_3'] ?></p>
                                             <p><?php echo "d) " . $questao['alt_4'] ?></p>
-                                            <p><b>Resposta: <?php if($questao['alt_correta'] == "alt1"){ 
-                                                echo $questao['alt_1']; 
-                                                } elseif($questao['alt_correta'] == "alt2") {
-                                                    echo $questao['alt_2'];
-                                                } elseif($questao['alt_correta'] == "alt3"){
-                                                    echo $questao['alt_3'];
-                                                } else {
-                                                    echo $questao['alt_4'];
-                                                }?></b></p>
+                                            <p><b>Resposta: <?php if ($questao['alt_correta'] == "alt1") {
+                                                                echo $questao['alt_1'];
+                                                            } elseif ($questao['alt_correta'] == "alt2") {
+                                                                echo $questao['alt_2'];
+                                                            } elseif ($questao['alt_correta'] == "alt3") {
+                                                                echo $questao['alt_3'];
+                                                            } else {
+                                                                echo $questao['alt_4'];
+                                                            } ?></b></p>
                                         </div>
                                     </div>
-                                </div>    
+                                </div>
                             </div>
                             <?php if ($count == $limite) :
-                                    echo '</div>';
-                                    echo '<div class="row">';
-                                    $count = 0;
-                                    endif ?>
+                                echo '</div>';
+                                echo '<div class="row">';
+                                $count = 0;
+                            endif ?>
                         <?php endforeach ?>
                     </div>
                     <div class="row">
                         <div class="col">
                             <div class="text-end py-3">
-                                <button class="btn link-body-emphasis text-light" type="submit" id="cadastro" name="cadastrar" style="background-color: var(--color-purple);">Cadastrar</button>
+                                <button class="btn link-body-emphasis text-light" type="submit" id="cadastro" name="cadastrar" style="background-color: var(--color-purple);" disabled>Cadastrar</button>
                                 <a class="btn link-body-emphasis text-light" href="list_questionario.php" style="background-color: var(--color-blue);">Voltar</a>
                             </div>
                         </div>
@@ -113,23 +113,24 @@ $questoes = mysqli_fetch_all($result, MYSQLI_ASSOC);
             </div>
         </div>
     </main>
-    <?php include_once('rodape.php')?>
+    <?php include_once('rodape.php') ?>
     <script>
         function limitar(){
-            let botaoEnvio = document.querySelectorAll("#cadastro");
+            let botaoEnvio = document.getElementById("cadastro");
             let checkBoxes = document.querySelectorAll("#checkbox");
             let limite = 10;
             let selecionado = 0;
-            for(count = 0; count < checkBoxes.length; count++){
-                if(checkBoxes[count].checked){
-                    selecionado++;
-                } else {
-                    selecionado--;
-                }
-                if(selecionado > limite || selecionado < limite){
-                    botaoEnvio.disabled = true;
-                } else {
-                    botaoEnvio.disabled = false;
+            for (count = 0; count < checkBoxes.length; count++) {
+                //console.log(checkBoxes[count]);
+                if (checkBoxes[count].checked == true) {
+                    selecionado = selecionado + 1;
+                    console.log(selecionado);
+                    if (selecionado > limite || selecionado < limite) {
+                        botaoEnvio.disabled = true;
+
+                    } else {
+                        botaoEnvio.disabled = false;
+                    }
                 }
             }
         }
