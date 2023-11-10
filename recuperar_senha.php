48311d4838254516d9de716497b97baa5fcd716f
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-11-09 23:02:38 -0300

    Recuperação de senha
    Concluído com sucesso!

--- recuperar_senha.php
+++ recuperar_senha.php
@@ -1,19 +1,52 @@
 <?php
 include_once('conexao.php');
 $conexao = conectar();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+
 if (isset($_POST['solicitar'])) {
     $nova_senha = substr(md5(time()), 0, 6);
-    $nova_senha_cript = md5(md5($nova_senha));
     $email = $_POST['email'];
+    $nome = $_POST['nome'];
 
     $sql = "SELECT * FROM usuario WHERE email = '$email' LIMIT 1";
     $resultado = mysqli_query($conexao, $sql);
 
     if ($resultado == TRUE) {
-        if (mysqli_num_rows($resultado) > 0) {
-            $sql = "UPDATE usuario SET senha = '$nova_senha_cript' WHERE email = '$email'";
+        if (mysqli_num_rows($resultado) == 1) {
+            $sql = "UPDATE usuario SET senha = '$nova_senha' WHERE email = '$email'";
             $resultado = mysqli_query($conexao, $sql);
             if ($resultado == TRUE) {
+                try {
+                    $mail = new PHPMailer();
+                    $mail->IsSMTP();
+                    $mail->Host = "smtp.gmail.com";
+                    $mail->SMTPAuth = true;
+                    $mail->Username = 'cafecrimesecasos@gmail.com'; 
+                    $mail->Password = 'brqubszmzpjfcrji';
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                    $mail->Port = 465;
+                    
+                    $mail->setFrom('cafecrimesecasos@gmail.com', 'C3 - Café, Crimes e Casos'); 
+                    $mail->addAddress($email, $nome);
+                    $mail->addReplyTo('cafecrimesecasos@gmail.com');
+
+                    $mail->IsHTML(true); 
+                    $mail->Subject = "Recuperar senha de acesso do C3";
+
+                    $mensagem = "<h3>Olá ". $nome ."</h3>Uma solicitação de recuperação de senha foi realizado na plataforma C3 - Café, Crimes e Casos.<br>Sua senha temporária é " . $nova_senha . ".<br>Assim que você acessar sua conta na plataforma, solicitamos que vá na aba <b>Editar Conta</b> e altere a senha para uma senha mais segura.<br><br>Para mais informações, entre em contato conosco!";
+
+                    $mail->Body = $mensagem;
+                    $mail->send();
+                    $msg = 2;
+                } catch (Exception $e) {
+                    echo "Erro ao enviar o email: {$mail->ErrorInfo}";
+                }
+
             } else {
                 echo mysqli_errno($conexao) . ": " . mysqli_error($conexao);
                 die();
@@ -46,7 +79,7 @@ if (isset($_POST['solicitar'])) {
         <div class="row">
             <div class="col">
                 <div class="text-center py-3">
-                    <h1>Recuperar a Senha</h1>
+                    <h2>Recuperar a Senha</h2>
                     <div class="m-3"><i class="fa-solid fa-lock fa-2xl" style="color: #000000;"></i></div>
                     <hr>
                 </div>
@@ -58,17 +91,20 @@ if (isset($_POST['solicitar'])) {
                     <button class="btn-close" data-bs-dismiss="alert"></button>
                     O email fornecido não existe em nosso banco!
                 </div>
-            <?php } else if ($msg == 2) { ?>
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <?php } elseif($msg == 2) { ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
                     <button class="btn-close" data-bs-dismiss="alert"></button>
-                    As senhas devem ser iguais!
+                    Email enviado com sucesso, visualize sua caixa de mensagem!
                 </div>
-        <?php }
-        endif ?>
+            <?php } endif ?>
         <div class="row">
             <div class="col">
                 <form method="POST" action="" class="">
                     <div class="row">
+                        <div class="col-xl-12 py-2">
+                            <label class="form-label">Nome:</label>
+                            <input class="form-control" type="text" name="nome" class="input" required>
+                        </div>
                         <div class="col-xl-12">
                             <label class="form-label">Email:</label>
                             <input class="form-control" type="email" name="email" class="input" required>
